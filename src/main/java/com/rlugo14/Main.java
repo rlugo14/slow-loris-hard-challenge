a623993ce32f2a08d5c2cafe63bb778fc4610f3f
Raul Lugo <lugo@sipgate.de>
2019-07-26 09:23:40 +0200

    🎉 Initial commit

---
+++ src/main/java/com/rlugo14/Main.java
@@ -0,0 +1,6 @@
+package com.rlugo14;
+
+public class Main {
+	
+	public static void main(String[] args) {}
+}
---
+++ src/main/java/com/rlugo14/Scale.java
@@ -0,0 +1,10 @@
+package com.rlugo14;
+
+import java.util.List;
+
+public class Scale {
+	
+	static List<List<Integer>> getMasses(Integer slowLorisWeight, List<Integer> allMasses) {
+	
+	}
+}
